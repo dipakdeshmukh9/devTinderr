1d560812487d492403a1c5be9639641daca6eb62
Dipak Deshmukh <121744826+dipakdeshmukh9@users.noreply.github.com>
2026-02-14 06:49:45 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req , res) => {
+app.get("/user/:userId/:name/:password", (req , res) => {
+    console.log(req.params);
     res.send({firstName: "Dipak", lastName: "Deshmukh"});
-});
-app.post("/user", (req , res) => {
-    res.send("data successfully added to the database");
-});
-app.delete("/user", (req , res) => {
-    res.send("data successfully deleted from the database");
-});
-app.use("/test", (req , res) => {
-    res.send("Hell0 form the server");
 });
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
